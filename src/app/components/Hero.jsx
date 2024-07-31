e73d665894f2077e071b5d04ce07f7be206e4f8a
busra <brordu61@gmail.com>
2024-07-31 23:01:20 +0300

    hero tamamlandı

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,23 +1,44 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
+
 const Hero = () => {
   return (
     <section>
-        <div className='grid grid-cols-1 lg:grid-cols-12'>
-            <div className='col-span-7 place-self-center'>
-                <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-                    Merhaba, ben büşra
+        <div className='grid grid-cols-1 sm:grid-cols-12'>
+            <div className='col-span-7 place-self-center text-center sm:text-left'>
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400  to-pink-600  '>
+                        Merhaba, Ben {""}
+                        </span>
+                        <br />
+                        <TypeAnimation
+                        sequence={[
+                        'Büşra',
+                        1000, 
+                        'Bir Frontend Web Developerım',
+                        1000,
+                        've bunun üzerine Acunmedya Akademide eğitim alıyorum',
+                        1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                    />
                 </h1> 
-                <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing 
-                    elit. Eveniet repellat est ipsam mollitia harum. 
-                    Consequatur accusamus dolore optio qui ipsa 
-                    quibusdam obcaecati veniam dolores sequi placeat. 
-                    Explicabo molestiae laborum earum.
+                <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
+                    Frontend developer olmak üzere eğitim alıyorum. 
+                    Gelecekte 3D websiteler ve oyunlar yapmak istiyorum. 
+                    Ayrıca bir dijital göçmen olmak istiyorum. 
+                    Bu serüvende bana destek olun. 
+                     
                 </p>
                 <div>
-                    <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Beni işe al</button>
-                    <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
+                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Linkedin</button>
+                    <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3'>
+                        <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+                        </button>
                 </div>
             </div>
             <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,11 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+      <div class="container mx-auto px-12 py-4">
       <Header/>
       <Hero/>
+      </div>
     </main>
   );
 }
