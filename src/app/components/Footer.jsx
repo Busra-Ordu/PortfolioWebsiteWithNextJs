703e3c4d7e717c09812cbfd74d78416c68355e1f
busra <brordu61@gmail.com>
2024-08-02 13:44:41 +0300

    project kısmı tamamlanmadı footer yazı eklendi

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,11 @@ const Footer = () => {
   return (
     <section>
       <div className=' flex flex-row justify-center items-center gap-2 py-6'>
-      <h1 className='text-white font-bold'>Busrası footer</h1>
+      <h1 className='text-white font-bold'>
+      © 2024 Portfolio <br />
+      Footer  <br />
+      Tüm hakları saklıdır
+        </h1>
       </div>
     </section>
   )
---
+++ src/app/components/ProjectTag.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProjectTag = ({name,onClick, isSelected}) => {
+  return (
+    <button className='rounded-full border-2 border-purple-500 px-6 
+    py-3 text-xl cursor-pointer '>
+          Hepsi
+        </button>
+  )
+}
+
+export default ProjectTag
