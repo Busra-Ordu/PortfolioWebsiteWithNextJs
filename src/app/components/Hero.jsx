f0e55e2a54df9df14f07a8de7f036d75413b0de4
busra <brordu61@gmail.com>
2024-08-03 15:39:40 +0300

    cv indirme eklendi favicon değiştirildi

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -40,9 +40,10 @@ const Hero = () => {
                     <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Linkedin</button>
                     </Link>
                     
+                    <a href="/ozgecmis.pdf" download>
                     <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3'>
                         <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
-                        </button>
+                    </button></a>
                 </div>
             </div>
             <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
